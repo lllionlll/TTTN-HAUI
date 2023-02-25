6866adc4384b032884053f7fe63c9b6053f9bfe8
lllionlll <lam14.dev@gmail.com>
2023-02-25 21:25:30 +0700

    Tuan 5

--- zalo/lib/common/navigate.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-Future<void> navigate(BuildContext context, Widget widget) async {
-  Navigator.of(context).pushReplacement(
-      MaterialPageRoute(
-        builder: (context) => widget,
-      )
-  );
-}
--- zalo/lib/constants/constants.dart
+++
@@ -1,8 +0,0 @@
-
-class Constants {
-  static const String CREAT_SUCCESS = 'Tạo tài khoản thành công!';
-  static const String SIGN_IN_SUCCESS = 'Đăng nhập thành công!';
-  static const String WEAK_PASSWORD = 'Mật khẩu yếu!';
-  static const String EMAIL_IS_READY = 'Email này đã tồn tại!';
-  static const String ACCOUNT_NOT_CORRECT = 'Tài khoản không tồn tại!';
-}
